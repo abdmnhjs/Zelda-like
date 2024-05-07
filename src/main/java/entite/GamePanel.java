f61bbe912feeb3948007f7eb7c6f86bdefa5b6fd
Eric De Tommaso <eric.detommaso@gmail.com>
2024-05-07 11:44:18 +0200

    Test

--- src/main/java/entite/GamePanel.java
+++ src/main/java/entite/GamePanel.java
@@ -3,6 +3,9 @@ package entite;
 import modele.Environnement;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicTreeUI;
+import java.awt.*;
+
 
 public class GamePanel extends JPanel implements Runnable {
 
@@ -15,10 +18,51 @@ public class GamePanel extends JPanel implements Runnable {
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
+    final int FPS =60;
+
+    Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
+    bgqiugrnqjgrnjkg;
 
 
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.cyan);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+
+    }
+
+    public void startGameThread(){
+        Thread gameThread = new Thread(this);
+        gameThread.start();
+    }
     @Override
     public void run() {
 
+        double intervale = 1000000000/FPS;
+        double delta = 0;
+        long tempsPrecedent = System.nanoTime();
+        long tempsActuel;
+
+        while (gameThread!=null){
+
+            tempsActuel=System.nanoTime();
+            delta+= (tempsActuel-tempsPrecedent)/intervale;
+            tempsPrecedent=tempsActuel;
+            if (delta>1){
+                update();
+                repaint();
+                delta--;
+            }
+        }
     }
+
+    public void update(){
+        if ()
+    }
+
+
 }
+
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.sae_zeldalike {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.desktop;
 
     opens com.example.sae_zeldalike to javafx.fxml;
     exports com.example.sae_zeldalike;
