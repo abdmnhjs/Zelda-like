d34d8c9badd4b36a2a01e210d64ae93d593cf40d
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-05-16 11:58:14 +0200

    autre syntaxe

--- src/main/java/com/example/sae_zeldalike/Clavier.java
+++ src/main/java/com/example/sae_zeldalike/Clavier.java
@@ -2,6 +2,7 @@ package com.example.sae_zeldalike;
 
 import com.example.sae_zeldalike.modele.Personnage;
 import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Clavier implements EventHandler<KeyEvent> {
@@ -14,23 +15,32 @@ public class Clavier implements EventHandler<KeyEvent> {
 
     @Override
     public void handle(KeyEvent keyEvent) {
-        switch (keyEvent.getCode()){
+        if(keyEvent.getCode() == KeyCode.Z){
+            personnage.setPositionYProperty(personnage.getPositionY() - personnage.getVitesseDeplacement());
+        }
+        if(keyEvent.getCode() == KeyCode.S){
+            personnage.setPositionYProperty(personnage.getPositionY() + personnage.getVitesseDeplacement());
+        }
+        if(keyEvent.getCode() == KeyCode.Q){
+            personnage.setPositionXProperty(personnage.getPositionX() - personnage.getVitesseDeplacement());
+        }
+        if(keyEvent.getCode() == KeyCode.D){
+            personnage.setPositionXProperty(personnage.getPositionX() + personnage.getVitesseDeplacement());
+        }
+
+        /*switch (keyEvent.getCode()){
             case Z -> {
                 personnage.setPositionYProperty(personnage.getPositionY() - personnage.getVitesseDeplacement());
-                System.out.println("haut : " + this.personnage.getPositionY());
             }
             case S -> {
                 personnage.setPositionYProperty(personnage.getPositionY() + personnage.getVitesseDeplacement());
-                System.out.println("bas : " + this.personnage.getPositionY());
             }
             case Q -> {
                 personnage.setPositionXProperty(personnage.getPositionX() - personnage.getVitesseDeplacement());
-                System.out.println("droite : " + this.personnage.getPositionX());
             }
             case D -> {
                 personnage.setPositionXProperty(personnage.getPositionX() + personnage.getVitesseDeplacement());
-                System.out.println("gauche : " + this.personnage.getPositionX());
             }
-        }
+        }*/
     }
 }
