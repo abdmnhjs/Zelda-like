fa258b2c265e106160c352fb245ed0dee47925a5
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-05-22 09:30:16 +0200

    recup classe Clavier

--- src/main/java/com/example/sae_zeldalike/Controlleur/Clavier.java
+++ src/main/java/com/example/sae_zeldalike/Controlleur/Clavier.java
@@ -1,76 +1,78 @@
 package com.example.sae_zeldalike.Controlleur;
 
-import com.example.sae_zeldalike.modele.Environnement.Map;
 import com.example.sae_zeldalike.modele.Personnage.*;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableSet;
 import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
 public class Clavier implements EventHandler<KeyEvent> {
 
     private Personnage personnage;
-    private Map map;
 
-    public Clavier(Personnage personnage, Map map) {
-        this.personnage = personnage;
-        this.map = map;
+
+    public Clavier(Personnage p) {
+        this.personnage = p;
     }
 
     @Override
     public void handle(KeyEvent keyEvent) {
-        evenementTouche(keyEvent);
+        interactionTouche(keyEvent);
     }
 
-    private void evenementTouche(KeyEvent keyEvent) {
-        int positionX = personnage.getPositionX();
-        int positionY = personnage.getPositionY();
-        int vitesseDeplacement = personnage.getVitesseDeplacement();
-        int newX = positionX;
-        int newY = positionY;
 
+    private void interactionTouche(KeyEvent keyEvent) {
+
+        int newX;
+        int newY;
         switch (keyEvent.getCode()) {
-            case Z -> newY -= vitesseDeplacement;
-            case S -> newY += vitesseDeplacement;
-            case Q -> newX -= vitesseDeplacement;
-            case D -> newX += vitesseDeplacement;
-        }
+            case Z -> {
+                newX = personnage.getPositionX();
+                newY = (personnage.getPositionY()- personnage.getVitesseDeplacement());
+                if (personnage.getEnvironnement().estDansLimiteTerrain(newX, newY)) {
+                    personnage.setPositionYProperty(personnage.getPositionY() - personnage.getVitesseDeplacement());
+                    System.out.println("peut avancer");
+                }else {
+                    System.out.println("n'avance pas");
+                }
 
-        if (coordonneesPossibles(newX, newY)) {
-            personnage.setPositionXProperty(newX);
-            personnage.setPositionYProperty(newY);
-            System.out.println("Position X : " + personnage.getPositionX() + " Position Y : " + personnage.getPositionY());
+                personnage.setDirection("UP");
+
+            }
+            case S -> {
+                newX = personnage.getPositionX();
+                newY = personnage.getPositionY()+ personnage.getVitesseDeplacement();
+                if (personnage.getEnvironnement().estDevantObstacle(newX,newY)) {
+                    personnage.setPositionYProperty(personnage.getPositionY() + personnage.getVitesseDeplacement());
+                    System.out.println("peut avancer");
+                }
+                personnage.setDirection("DOWN");
+            }
+            case Q -> {
+                newX = personnage.getPositionX()- personnage.getVitesseDeplacement();
+                newY = personnage.getPositionY();
+                if (personnage.getEnvironnement().estDevantObstacle(newX,newY)) {
+                    personnage.setPositionXProperty(personnage.getPositionX() - personnage.getVitesseDeplacement());
+                }
+                personnage.setDirection("LEFT");
+            }
+            case D -> {
+                newX = personnage.getPositionX()+ personnage.getVitesseDeplacement();
+                newY = personnage.getPositionY();
+                if (personnage.getEnvironnement().estDevantObstacle(newX,newY)) {
+                    personnage.setPositionXProperty(personnage.getPositionX() + personnage.getVitesseDeplacement());
+                }
+                personnage.setDirection("RIGHT");
+
+            }
         }
+        System.out.println("Position X : " + personnage.getPositionX() + " Position Y : " + personnage.getPositionY());
+
     }
 
-    private boolean coordonneesPossibles(int newX, int newY) {
-        boolean withinBounds = newX >= 0 && newY >= 0 && newX < map.getColonne() * 32 && newY < map.getLigne() * 32;
-        if (!withinBounds) {
-            return false;
-        }
 
-        int tileSize = 32;
-        int topLeftX = newX;
-        int topLeftY = newY;
-        int bottomRightX = newX + tileSize - 1;
-        int bottomRightY = newY + tileSize - 1;
-
-        boolean topLeft = isTileWalkable(topLeftX, topLeftY);
-        boolean bottomRight = isTileWalkable(bottomRightX, bottomRightY);
-
-        return topLeft && bottomRight;
-    }
-
-    private boolean isTileWalkable(int x, int y) {
-        int tileSize = 32;
-        int gridX = x / tileSize;
-        int gridY = y / tileSize;
-
-        // Ajustement de l'index pour éviter le décalage
-        int index = gridY * map.getColonne() + gridX;
-        System.out.println("INDEX : " + index);
-        if (index < 0 || index >= map.getMap().length) {
-            return false;
-        }
-
-        return map.getMap()[index] != 54;
-    }
 }
