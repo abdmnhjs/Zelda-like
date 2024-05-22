946febd7e760b12db817d4cc4011c6c66a9c5625
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-05-22 16:17:04 +0200

    collisions marchent mais pas trop à droite

--- src/main/java/com/example/sae_zeldalike/Controlleur/Clavier.java
+++ src/main/java/com/example/sae_zeldalike/Controlleur/Clavier.java
@@ -1,60 +1,60 @@
-package com.example.sae_zeldalike.Controlleur;
+    package com.example.sae_zeldalike.Controlleur;
 
-import com.example.sae_zeldalike.modele.Personnage.*;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.collections.ObservableSet;
-import javafx.event.EventHandler;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+    import com.example.sae_zeldalike.modele.Personnage.*;
+    import javafx.beans.property.SimpleStringProperty;
+    import javafx.beans.property.StringProperty;
+    import javafx.collections.FXCollections;
+    import javafx.collections.ObservableList;
+    import javafx.collections.ObservableSet;
+    import javafx.event.EventHandler;
+    import javafx.scene.input.KeyCode;
+    import javafx.scene.input.KeyEvent;
 
-public class Clavier implements EventHandler<KeyEvent> {
+    public class Clavier implements EventHandler<KeyEvent> {
 
-    private Personnage personnage;
-
-
-    public Clavier(Personnage p) {
-        this.personnage = p;
-    }
-
-    @Override
-    public void handle(KeyEvent keyEvent) {
-        interactionTouche(keyEvent);
-    }
+        private Personnage personnage;
 
 
-    private void interactionTouche(KeyEvent keyEvent) {
-
-        switch (keyEvent.getCode()) {
-            case Z -> {
-                int newY = this.personnage.getPositionY() - this.personnage.getVitesseDeplacement();
-                if (!this.personnage.getEnvironnement().estDevantObstacle(this.personnage.getPositionX(), newY)) {
-                    this.personnage.setPositionYProperty(newY);
-                }
-            }
-            case S -> {
-                int newY = this.personnage.getPositionY() + this.personnage.getVitesseDeplacement();
-                if (!this.personnage.getEnvironnement().estDevantObstacle(this.personnage.getPositionX(), newY)) {
-                    this.personnage.setPositionYProperty(newY);
-                }
-            }
-            case D -> {
-                int newX = this.personnage.getPositionX() + this.personnage.getVitesseDeplacement();
-                if (!this.personnage.getEnvironnement().estDevantObstacle(newX, this.personnage.getPositionY())) {
-                    this.personnage.setPositionXProperty(newX);
-                }
-            }
-            case Q -> {
-                int newX = this.personnage.getPositionX() - this.personnage.getVitesseDeplacement();
-                if (!this.personnage.getEnvironnement().estDevantObstacle(newX, this.personnage.getPositionY())) {
-                    this.personnage.setPositionXProperty(newX);
-                }
-            }
+        public Clavier(Personnage p) {
+            this.personnage = p;
         }
-        System.out.println("Position X : " + personnage.getPositionX() + " Position Y : " + personnage.getPositionY());
+
+        @Override
+        public void handle(KeyEvent keyEvent) {
+            interactionTouche(keyEvent);
+        }
+
+
+        private void interactionTouche(KeyEvent keyEvent) {
+
+            switch (keyEvent.getCode()) {
+                case Z -> {
+                    int newY = this.personnage.getPositionY() - this.personnage.getVitesseDeplacement();
+                    if (!this.personnage.getEnvironnement().estDevantObstacle(this.personnage.getPositionX(), newY)) {
+                        this.personnage.setPositionYProperty(newY);
+                    }
+                }
+                case S -> {
+                    int newY = this.personnage.getPositionY() + this.personnage.getVitesseDeplacement();
+                    if (!this.personnage.getEnvironnement().estDevantObstacle(this.personnage.getPositionX(), newY)) {
+                        this.personnage.setPositionYProperty(newY);
+                    }
+                }
+                case D -> {
+                    int newX = this.personnage.getPositionX() + this.personnage.getVitesseDeplacement();
+                    if (!this.personnage.getEnvironnement().estDevantObstacle(newX, this.personnage.getPositionY())) {
+                        this.personnage.setPositionXProperty(newX);
+                    }
+                }
+                case Q -> {
+                    int newX = this.personnage.getPositionX() - this.personnage.getVitesseDeplacement();
+                    if (!this.personnage.getEnvironnement().estDevantObstacle(newX, this.personnage.getPositionY())) {
+                        this.personnage.setPositionXProperty(newX);
+                    }
+                }
+            }
+            System.out.println("Position X : " + personnage.getPositionX() + " Position Y : " + personnage.getPositionY());
 
 
 
-    }}
+        }}
