a8d5bd2931aacb992bac40d7c487f9cf2d1883c6
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-05-23 15:02:44 +0200

    ajout arrayliste pr tt les coordonnées des tuiles

--- src/main/java/com/example/sae_zeldalike/modele/Environnement/Map.java
+++ src/main/java/com/example/sae_zeldalike/modele/Environnement/Map.java
@@ -7,6 +7,7 @@ public class Map {
     private int[][] map;
     private int ligne;
     private int colonne;
+    private ArrayList<int[]> coordonnéesToutesTuiles;
     private ArrayList<int[]> coordonnéesTuilesTraversables;
     private ArrayList<int[]> coordonnéesTuilesNonTraversables;
 
@@ -35,6 +36,7 @@ public class Map {
         this.ligne=17;
         this.colonne=34;
 
+        this.coordonnéesToutesTuiles = new ArrayList<>();
         this.coordonnéesTuilesTraversables = new ArrayList<>();
         this.coordonnéesTuilesNonTraversables = new ArrayList<>();
 
@@ -42,8 +44,10 @@ public class Map {
             for(int x = 0 ; x < this.map[y].length ; x++){
                 if(this.map[y][x] != 54){
                     this.coordonnéesTuilesTraversables.add(new int[]{y * 32, x * 32});
+                    this.coordonnéesToutesTuiles.add(new int[]{y * 32, x * 32});
                 } else {
                     this.coordonnéesTuilesNonTraversables.add(new int[]{y * 32, x * 32});
+                    this.coordonnéesToutesTuiles.add(new int[]{y * 32, x * 32});
                 }
             }
         }
