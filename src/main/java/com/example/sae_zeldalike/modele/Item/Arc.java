099a72ae7548c2c2c79d736fa67584794210201b
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-05-27 09:50:14 +0200

    classe Flèche remplie vite fait

--- src/main/java/com/example/sae_zeldalike/modele/Item/Arc.java
+++ src/main/java/com/example/sae_zeldalike/modele/Item/Arc.java
@@ -1,4 +1,7 @@
-//package com.example.sae_zeldalike.modele.Item;
+package com.example.sae_zeldalike.modele.Item;
 
-//public class Arc extends Arme{
-//}
+import java.util.ArrayList;
+
+public class Arc extends Arme{
+    private ArrayList<Flèche> flèches;
+}
--- src/main/java/com/example/sae_zeldalike/modele/Item/Flèche.java
+++ src/main/java/com/example/sae_zeldalike/modele/Item/Flèche.java
@@ -1,4 +1,11 @@
 package com.example.sae_zeldalike.modele.Item;
 
+import javafx.beans.property.IntegerProperty;
+
 public class Flèche {
+    private IntegerProperty xProperty;
+    private IntegerProperty yProperty;
+    private int dégâts;
+    private IntegerProperty vitesseProperty;
+
 }
