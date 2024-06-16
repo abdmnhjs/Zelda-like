2521d781fbebf48c171ecee23a2c7fcd22c24de4
Eric DE TOMMASO <eric.detommaso@gmail.com>
2024-06-16 23:55:56 +0200

    finalement ca bug

--- src/main/java/com/example/sae_zeldalike/modele/Personnage/Ennemi/Magicien.java
+++ src/main/java/com/example/sae_zeldalike/modele/Personnage/Ennemi/Magicien.java
@@ -4,6 +4,8 @@ import com.example.sae_zeldalike.modele.Environnement.Environnement;
 import com.example.sae_zeldalike.modele.Personnage.Personnage;
 import javafx.beans.property.IntegerProperty;
 
+import java.util.Random;
+
 public class Magicien extends Ennemi {
 
     private Environnement environnement;
@@ -26,22 +28,28 @@ public class Magicien extends Ennemi {
 
         if (compteur < 5 && this.estVivant()) {
 
-            int[][] coordonnées = {{50, 150}, {150, 250}, {350, 150}, {150, 50}, {100, 150}};
-
-            for (int i = 0; i < 5; i++) {
+            for (int i = compteur; i < 5; i++) {
 
                 Ennemi2 ennemiAjouté = new Ennemi2(environnement);
 
-                int positionX = coordonnées[i][0];
-                int positionY = coordonnées[i][1];
+//                genererPositionAleatoires(ennemiAjouté);
 
-                ennemiAjouté.setPositionXProperty(positionX);
-                ennemiAjouté.setPositionYProperty(positionY);
-
-                environnement.ajouterPersonnage(ennemiAjouté);
+                getEnvironnement().ajouterPersonnage(ennemiAjouté);
 
             }
         }
+    }
+
+    public void genererPositionAleatoires(Personnage p){
+        Random random = new Random();
+        int posX,posY;
+        do{
+            posX = (random.nextInt(101))-50;
+            posY = (random.nextInt(101))-50;
+
+        }while (!getEnvironnement().estDansTuile(11,p.hitbox(getPositionX()+posX,getPositionY()+posY)));
+        p.setPositionXProperty(posX);
+        p.setPositionYProperty(posY);
     }
 
 }
