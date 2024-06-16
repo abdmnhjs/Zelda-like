082c9edbfe3558be7f7324f10ac4fa1216ef0afc
Abdelaziz Chaaraoui <chaaraouiabdelaziz@gmail.com>
2024-06-16 23:24:03 +0200

    pas fini

--- src/test/java/com/example/sae_zeldalike/modele/Environnement/EnvironnementTest.java
+++ src/test/java/com/example/sae_zeldalike/modele/Environnement/EnvironnementTest.java
@@ -1,6 +1,14 @@
 package com.example.sae_zeldalike.modele.Environnement;
 
+import com.example.sae_zeldalike.modele.Hitbox;
+import com.example.sae_zeldalike.modele.Item.Item;
+import com.example.sae_zeldalike.modele.Item.NonStockable.CoeurRouge;
+import com.example.sae_zeldalike.modele.Item.StockableDansInventaire.Arme.Arc;
+import com.example.sae_zeldalike.modele.Personnage.Ennemi.Ennemi1;
 import com.example.sae_zeldalike.modele.Personnage.Link;
+import com.example.sae_zeldalike.modele.Personnage.Personnage;
+import com.example.sae_zeldalike.modele.Projectile.Fleche;
+import com.example.sae_zeldalike.modele.Projectile.Projectile;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Test;
@@ -14,80 +22,197 @@ class EnvironnementTest {
     void ajouterLink() throws IOException {
         Map map = new Map("src/main/resources/terrain2.json");
         Environnement environnement = new Environnement(map);
-        ObservableList<Link> observableListLink = FXCollections.observableArrayList();
         Link link = new Link(environnement);
-        // Ajouter le lien à la liste observable
-        observableListLink.add(link);
-        // Vérifier si la liste observable contient le lien ajouté
-        assertTrue(observableListLink.contains(link));
+
+        environnement.ajouterLink(link);
+
+        assertTrue(environnement.getLinkRemovalQueue().contains(link));
+    }
+
+
+
+    @Test
+    void ajouterPersonnage() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Personnage personnage = new Ennemi1(environnement);
+
+        environnement.ajouterPersonnage(personnage);
+
+        assertTrue(environnement.getPersonnages().contains(personnage));
     }
 
 
     @Test
-    void ajouterPersonnage() {
+    void ajouterItem() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Item item = new CoeurRouge(environnement);
+
+        environnement.ajouterItem(item);
+
+        assertTrue(environnement.getItems().contains(item));
+    }
+
+
+    @Test
+    void supprimerItem() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Item item = new CoeurRouge(environnement);
+
+        environnement.ajouterItem(item);
+        environnement.supprimerItem(item);
+
+        assertFalse(environnement.getItems().contains(item));
+    }
+
+
+    @Test
+    void supprimerProjectiles() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Projectile projectile;
+        projectile = new Fleche(environnement, 15, 15, 32, 32, 30, new Arc(environnement), "UP");
+        environnement.ajouterProjectiles(projectile);
+        environnement.supprimerProjectiles(projectile);
+        assertFalse(environnement.getProjectiles().contains(projectile));
+    }
+
+
+    @Test
+    void supprimerLink() throws IOException {
+        Map map = new Map("file:src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Link link = new Link(environnement);
+        environnement.ajouterLink(link);
+        environnement.supprimerLink(link);
+        assertFalse(environnement.getLinkRemovalQueue().contains(link));
+    }
+
+
+    @Test
+    void supprimerPersonnage() throws IOException {
+        Map map = new Map("file:src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Personnage personnage = new Ennemi1(environnement);
+        environnement.ajouterPersonnage(personnage);
+        environnement.supprimerPersonnage(personnage);
+        assertFalse(environnement.getPersonnages().contains(personnage));
+    }
+
+
+    @Test
+    void ajouterProjectiles() throws IOException {
+        Map map = new Map("file:src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Projectile projectile = new Fleche(environnement, 15, 15, 32, 32, 30, 30, "UP");
+        environnement.ajouterProjectiles(projectile);
+        assertTrue(environnement.getProjectiles().contains(projectile));
+    }
+
+
+    @Test
+    void getProjectiles() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Projectile projectile = new Fleche(environnement, 15, 15, 32, 32, 30, new Arc(environnement), "UP");
+        environnement.ajouterProjectiles(projectile);
+
+        ObservableList<Projectile> projectiles = environnement.getProjectiles();
+        assertNotNull(projectiles);
+        assertTrue(projectiles.contains(projectile));
     }
 
     @Test
-    void ajouterItem() {
+    void getItems() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Item item = new CoeurRouge(environnement);
+        environnement.ajouterItem(item);
+
+        ObservableList<Item> items = environnement.getItems();
+        assertNotNull(items);
+        assertTrue(items.contains(item));
     }
 
     @Test
-    void supprimerItem() {
+    void getPersonnages() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Personnage personnage = new Ennemi1(environnement);
+        environnement.ajouterPersonnage(personnage);
+
+        ObservableList<Personnage> personnages = environnement.getPersonnages();
+        assertNotNull(personnages);
+        assertTrue(personnages.contains(personnage));
     }
 
     @Test
-    void supprimerProjectiles() {
+    void getMap() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        assertEquals(map, environnement.getMap());
     }
 
     @Test
-    void supprimerLink() {
+    void estDevantObstacle() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Hitbox hitbox = new Hitbox(0, 0, 1, 1);
+
+        // Assume that tile (0, 0) is an obstacle
+        map.getMap()[0][0] = 54;
+
+        assertTrue(environnement.estDevantObstacle(hitbox));
+    }
+
+
+    @Test
+    void estDansTuile() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Hitbox hitbox = new Hitbox(0, 0, 1, 1);
+        map.getMap()[0][0] = 10;
+        assertTrue(environnement.estDansTuile(10, hitbox));
+    }
+
+
+    @Test
+    void estDansLaZone() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Hitbox hitbox1 = new Hitbox(0, 0, 1, 1);
+        Hitbox hitbox2 = new Hitbox(0, 0, 2, 2);
+        assertTrue(environnement.estDansLaZone(hitbox1, hitbox2));
+    }
+
+
+    @Test
+    void getId() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        assertNotNull(environnement.getId());
+        assertTrue(environnement.getId().startsWith("E"));
     }
 
     @Test
-    void supprimerPersonnage() {
+    void getLinkRemovalQueue() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        Link link = new Link(environnement);
+        environnement.ajouterLink(link);
+        ObservableList<Link> linkRemovalQueue = environnement.getLinkRemovalQueue();
+        assertNotNull(linkRemovalQueue);
+        assertTrue(linkRemovalQueue.contains(link));
     }
 
     @Test
-    void ajouterProjectiles() {
-    }
-
-    @Test
-    void getProjectiles() {
-    }
-
-    @Test
-    void getItems() {
-    }
-
-    @Test
-    void getPersonnages() {
-    }
-
-    @Test
-    void getMap() {
-    }
-
-    @Test
-    void estDevantObstacle() {
-    }
-
-    @Test
-    void estDansTuile() {
-    }
-
-    @Test
-    void estDansLaZone() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void getLinkRemovalQueue() {
-    }
-
-    @Test
-    void init() {
+    void init() throws IOException {
+        Map map = new Map("src/main/resources/terrain2.json");
+        Environnement environnement = new Environnement(map);
+        environnement.init();
+        assertTrue(environnement.getItems().size() > 0);
+        assertTrue(environnement.getPersonnages().size() > 0);
     }
 }
---
+++ src/test/java/com/example/sae_zeldalike/modele/Personnage/LinkTest.java
@@ -0,0 +1,152 @@
+package com.example.sae_zeldalike.modele.Personnage;
+
+import com.example.sae_zeldalike.modele.Environnement.Environnement;
+import com.example.sae_zeldalike.modele.Inventaire;
+import com.example.sae_zeldalike.modele.Item.Item;
+import com.example.sae_zeldalike.modele.Item.NonStockable.CoeurRouge;
+import com.example.sae_zeldalike.modele.Item.NonStockable.Effet;
+import com.example.sae_zeldalike.modele.Item.StockableDansInventaire.Arme.Arme;
+import com.example.sae_zeldalike.modele.Item.StockableDansInventaire.Arme.Epée;
+import com.example.sae_zeldalike.modele.Item.StockableDansInventaire.Stockable;
+import javafx.beans.property.IntegerProperty;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LinkTest {
+
+    private Environnement environnement;
+    private Link link;
+
+    @Test
+    void getArmeEquiper() {
+        Epée epee = new Epée(environnement);
+        link.setArmeEquiper(epee);
+        assertEquals(epee, link.getArmeEquiper());
+    }
+
+    @Test
+    void setArmeEquiper() {
+        Epée epee = new Epée(environnement);
+        link.setArmeEquiper(epee);
+        assertEquals(epee, link.getArmeEquiper());
+    }
+
+    @Test
+    void ajouterBouclier() {
+        link.ajouterBouclier(10);
+        assertEquals(10, link.getPointDeVieAdditionelle());
+    }
+
+    @Test
+    void getPointDeVieAdditionelle() {
+        assertEquals(0, link.getPointDeVieAdditionelle());
+        link.ajouterBouclier(10);
+        assertEquals(10, link.getPointDeVieAdditionelle());
+    }
+
+    @Test
+    void getPointDeVieAdditionelleProperty() {
+        IntegerProperty property = link.getPointDeVieAdditionelleProperty();
+        assertNotNull(property);
+        assertEquals(0, property.get());
+        link.ajouterBouclier(10);
+        assertEquals(10, property.get());
+    }
+
+    @Test
+    void getPointDeVieMax() {
+        assertEquals(link.getPointDeVieAdditionelle() + link.getPointVie(), link.getPointDeVieMax());
+    }
+
+    @Test
+    void reduirePointsDeVie() {
+        link.ajouterBouclier(10);
+        link.reduirePointsDeVie(5);
+        assertEquals(5, link.getPointDeVieAdditionelle());
+        assertEquals(100, link.getPointVie());
+
+        link.reduirePointsDeVie(10);
+        assertEquals(0, link.getPointDeVieAdditionelle());
+        assertEquals(95, link.getPointVie());
+    }
+
+    @Test
+    void getInventaire() {
+        Inventaire inventaire = link.getInventaire();
+        assertNotNull(inventaire);
+    }
+
+    @Test
+    void utiliserItemDansInventaire() {
+        Epée epee = new Epée(environnement);
+        link.getInventaire().modifieStockableInventaire(epee);
+        link.utiliserItemDansInventaire();
+        assertEquals(epee, link.getArmeEquiper());
+    }
+
+    @Test
+    void emplacementInventaireLibre() {
+        assertTrue(link.emplacementInventaireLibre());
+    }
+
+    @Test
+    void modifieCaseInventaire() {
+        Stockable item = new Stockable() {
+            @Override
+            public Item getItem() {
+                return null;
+            }
+
+            @Override
+            public boolean effetUtiliser() {
+                return false;
+            }
+
+            @Override
+            public void utiliserCapacite() {
+
+            }
+        };
+        link.modifieCaseInventaire(item);
+        assertEquals(item, link.getInventaire().getInventaireCaseActuel());
+    }
+
+    @Test
+    void essaiRamasserItem() {
+        Item item = new CoeurRouge(environnement);
+        item.setPositionX(link.getPositionX());
+        item.setPositionY(link.getPositionY());
+        environnement.ajouterItem(item);
+        assertEquals(item, link.essaiRamasserItem());
+    }
+
+    @Test
+    void getEffets() {
+        assertNotNull(link.getEffets());
+    }
+
+    @Test
+    void ajouterEffet() {
+        Effet effet = new Effet() {
+            @Override
+            public void appliquer(Personnage personnage) {
+
+            }
+        };
+        link.ajouterEffet(effet);
+        assertTrue(link.getEffets().contains(effet));
+    }
+
+    @Test
+    void getPortefeuilleProperty() {
+        IntegerProperty portefeuille = link.getPortefeuilleProperty();
+        assertNotNull(portefeuille);
+    }
+
+    @Test
+    void ajouterPiece() {
+        link.ajouterPiece(10);
+        assertEquals(10, link.getPortefeuilleProperty().get());
+    }
+}
